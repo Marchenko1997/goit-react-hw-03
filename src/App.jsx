2c570e362cdf8cee3491360ff33905dedd1e0e68
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-03-15 19:25:58 +0100

    Commite last 5th task

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import { nanoid } from 'nanoid';
-import  { useState } from 'react';
+import  { useEffect, useState } from 'react';
 import "./App.css";
 
 function App() {
-  const [contacts, setContacts] =useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+  });
   const [filter, setFilter] = useState('');
+  
+  useEffect(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    if(savedContacts)
+    setContacts(JSON.parse(savedContacts))
+  }, []);
 
+  useEffect(() => {localStorage.setItem("contacts", JSON.stringify(contacts))} ,[contacts]);
   const deleteContact =(id) => {
     setContacts(prevContacts => prevContacts.filter(contact => contact.id !==id))
   }
@@ -28,7 +38,7 @@ function App() {
       name,
       number,
     };
-    setContacts([...contacts, newContact]);
+    setContacts(prevContacts => [...prevContacts, newContact]);
   };
 
   const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())
